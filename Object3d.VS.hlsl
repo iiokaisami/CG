f026640680a8e839398205345a036e424bc43bc3
iiokaisami <k023g0025@g.neec.ac.jp>
2024-06-19 11:45:25 +0900

    a

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -3,13 +3,16 @@
 struct TransformationMatrix
 {
     float4x4 WVP;
+    float4x4 World;
 };
+
 ConstantBuffer<TransformationMatrix> gTransformMatrix : register(b0);
 
 struct VertexShaderInput
 {
     float4 position : POSITION0;
     float2 texcoord : TEXCOORD0;
+    float3 normal : NORMAL0;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
@@ -17,6 +20,7 @@ VertexShaderOutput main(VertexShaderInput input)
     VertexShaderOutput output;
     output.position = mul(input.position, gTransformMatrix.WVP);
     output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float3x3) gTransformMatrix.World));
     return output;
 }
 
