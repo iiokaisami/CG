6b2adcb1848661df51fe8da10e4df0ce7da4a2fe
iiokaisami <k023g0025@g.neec.ac.jp>
2024-06-19 11:45:25 +0900

    a

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,11 +1,24 @@
+
+
+
+
 #include "Object3d.hlsli"
 
 struct Material
 {
     float4 color;
+    int enableLighting;
+};
+
+struct DirectionalLight
+{
+    float4 color;
+    float3 direction;
+    float intensity;
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
@@ -16,13 +29,20 @@ struct PixelShaderOutput
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
-
+    PixelShaderOutput output;
     float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     
-    PixelShaderOutput output;
-    output.color = gMaterial.color * textureColor;
+    if (gMaterial.enableLighting != 0)
+    {
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    }
+    else
+    {
+        output.color = gMaterial.color * textureColor;
+    }
     return output;
 }
-
 
 
