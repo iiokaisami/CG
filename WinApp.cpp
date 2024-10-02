2110e99aeaaba1d4a52be92066339739b76a0ad7
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-02 12:00:55 +0900

    03_02完成

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,9 +1,83 @@
 #include "WinApp.h"
 
+//ImGui
+#include "externals/imgui/imgui.h"
+#include "externals/imgui/imgui_impl_dx12.h"
+#include "externals/imgui/imgui_impl_win32.h"
+extern  IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+//ウィンドウプロシージャ
+LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam))
+	{
+		return true;
+	}
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
 void WinApp::Initialize()
 {
+	//COMの初期化
+	CoInitializeEx(0, COINIT_MULTITHREADED);
+
+
+	//ウィンドウを表示
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+	//クライアント領域のサイズ
+	const int32_t kClientWidth = 1200;
+	const int32_t kClientHeight = 720;
+
+	//ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
+
+	//クライアント領域をもとに実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+
+
+	//ウィンドウの設定
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,		//利用するクラス名
+		L"CG2",					//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
+		CW_USEDEFAULT,			//表示X座標(WindowsOSに任せる)
+		CW_USEDEFAULT,			//表示Y座標(WindowsOSに任せる)
+		wrc.right - wrc.left,	//ウィンドウ横幅
+		wrc.bottom - wrc.top,	//ウィンドウ縦幅
+		nullptr,				//親ウィンドウハンドル
+		nullptr,				//メニューハンドル
+		wc.hInstance,			//インスタンスハンドル
+		nullptr					//オプション
+	);
+
+
+
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
 }
 
 void WinApp::Update()
 {
+
 }
