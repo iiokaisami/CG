5d5afca5fe086bcd099e1d534149d69e19f2e5bb
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-02 12:33:04 +0900

    03_03完成

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,9 +1,6 @@
 #include "WinApp.h"
 
-//ImGui
-#include "externals/imgui/imgui.h"
-#include "externals/imgui/imgui_impl_dx12.h"
-#include "externals/imgui/imgui_impl_win32.h"
+
 extern  IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 //ウィンドウプロシージャ
@@ -29,8 +26,7 @@ void WinApp::Initialize()
 	CoInitializeEx(0, COINIT_MULTITHREADED);
 
 
-	//ウィンドウを表示
-	WNDCLASS wc{};
+	
 	//ウィンドウプロシージャ
 	wc.lpfnWndProc = WindowProc;
 	//ウィンドウクラス名
@@ -43,10 +39,6 @@ void WinApp::Initialize()
 	//ウィンドウクラスを登録する
 	RegisterClass(&wc);
 
-	//クライアント領域のサイズ
-	const int32_t kClientWidth = 1200;
-	const int32_t kClientHeight = 720;
-
 	//ウィンドウサイズを表す構造体にクライアント領域を入れる
 	RECT wrc = { 0,0,kClientWidth,kClientHeight };
 
@@ -56,9 +48,9 @@ void WinApp::Initialize()
 
 
 	//ウィンドウの設定
-	HWND hwnd = CreateWindow(
+	hwnd = CreateWindow(
 		wc.lpszClassName,		//利用するクラス名
-		L"CG2",					//タイトルバーの文字
+		L"CG",					//タイトルバーの文字
 		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
 		CW_USEDEFAULT,			//表示X座標(WindowsOSに任せる)
 		CW_USEDEFAULT,			//表示Y座標(WindowsOSに任せる)
