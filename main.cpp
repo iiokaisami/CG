f6e602c9e5edc2a68d1d58694593f97dbc2b7edd
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-02 11:59:52 +0900

    03_01完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <Windows.h>
 #include <cstdint>
-#include <windows.h>
 #include <string>
 #include <format>
 #include <d3d12.h>
@@ -22,6 +21,7 @@
 #include <wrl.h>
 
 #include "Input.h"
+#include "WinApp.h"
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -829,48 +829,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	D3DResourceLeakChecker leakCheck;
 
 
-	//COMの初期化
-	CoInitializeEx(0, COINIT_MULTITHREADED);
+	// ポインタ
+	WinApp* winApp = nullptr;
+
+	// WindowsAPIの初期化
+	winApp = new WinApp();
+	winApp->Initialize();
 
 	
-	//ウィンドウを表示
-	WNDCLASS wc{};
-	//ウィンドウプロシージャ
-	wc.lpfnWndProc = WindowProc;
-	//ウィンドウクラス名
-	wc.lpszClassName = L"CG2WindowClass";
-	//インスタンスハンドル
-	wc.hInstance = GetModuleHandle(nullptr);
-	//カーソル
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-
-	//ウィンドウクラスを登録する
-	RegisterClass(&wc);
-
-	//クライアント領域のサイズ
-	const int32_t kClientWidth = 1200;
-	const int32_t kClientHeight = 720;
-
-	//ウィンドウサイズを表す構造体にクライアント領域を入れる
-	RECT wrc = { 0,0,kClientWidth,kClientHeight };
-
-	//クライアント領域をもとに実際のサイズにwrcを変更してもらう
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-	//ウィンドウの設定
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,		//利用するクラス名
-		L"CG2",					//タイトルバーの文字
-		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
-		CW_USEDEFAULT,			//表示X座標(WindowsOSに任せる)
-		CW_USEDEFAULT,			//表示Y座標(WindowsOSに任せる)
-		wrc.right - wrc.left,	//ウィンドウ横幅
-		wrc.bottom - wrc.top,	//ウィンドウ縦幅
-		nullptr,				//親ウィンドウハンドル
-		nullptr,				//メニューハンドル
-		wc.hInstance,			//インスタンスハンドル
-		nullptr					//オプション
-	);
 
 	
 	//デバッグレイヤー
@@ -883,9 +849,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		debugController->SetEnableGPUBasedValidation(TRUE);
 	}
 #endif
-
-	//ウィンドウを表示する
-	ShowWindow(hwnd, SW_SHOW);
 
 
 	//DXGIファクトリーの生成
@@ -2005,6 +1968,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	// 入力解放
 	delete input;
+	// WindowsAPI解放
+	delete winApp;
 
 	CloseHandle(fenceEvent);
 	
