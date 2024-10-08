af1e98241c940639d35b36fa20d6f55db84d3430
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-08 16:58:07 +0900

    04_02完成

--- Logger.cpp
+++ Logger.cpp
@@ -1,9 +1,11 @@
 #include "Logger.h"
 
+#include <windows.h>
+
 namespace Logger
 {
 	void Logger::Log(const std::string& message)
 	{
-
+		OutputDebugStringA(message.c_str());
 	}
 }
--- Logger.h
+++ Logger.h
@@ -1,11 +1,11 @@
 #pragma once
+#include <string>
 
 
 // ログ出力
 namespace Logger
 {
 	void Log(const std::string& message);
-
 }
 
 class Logger{};
