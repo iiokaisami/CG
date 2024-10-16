1a5b3b59ec59ef9eff523580f7d470a1e4d653a3
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-16 13:00:08 +0900

    05_02完成

--- SpriteCommon.h
+++ SpriteCommon.h
@@ -1,15 +1,58 @@
 #pragma once
+#include <d3d12.h>
+#include "externals/DirectXTex/d3dx12.h"
+
+#include "DirectXCommon.h"
+#include "Logger.h"
 
 class SpriteCommon
 {
 public:
 
 	// 初期化
-	void Initialize();
+	void Initialize(DirectXCommon* dxCommon);
 
 	// ルートシグネイチャの作成
 	void CreateRootSignature();
 
 	// グラフィックスパイプラインの生成
+	void CreateGraphicsPipeline();
 
+	// 共通描画設定
+	void CommonDrawSetting();
+
+	//-----------ゲッター----------//
+
+	DirectXCommon* GetDxCommon() const { return dxCommon_; }
+
+private:
+
+	DirectXCommon* dxCommon_;
+
+	// デバイス
+	Microsoft::WRL::ComPtr<ID3D12Device> device_ = nullptr;
+	// コマンドリスト
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList_ = nullptr;
+
+	//ディスクリプタレンジの生成
+	D3D12_DESCRIPTOR_RANGE descriptorRange_[1]{};
+	//DepthStencilStateの設定
+	D3D12_DEPTH_STENCIL_DESC depthStencilDesc_{};
+	//InputLayout
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc_{};
+	//BlendStateの設定
+	D3D12_BLEND_DESC blendDesc_{};
+	//RasterizerStateの設定
+	D3D12_RASTERIZER_DESC rasterizerDesc_{};
+
+	//shaderをコンパイルする
+	Microsoft::WRL::ComPtr<IDxcBlob> vertexShaderBlob_ = nullptr;
+	Microsoft::WRL::ComPtr<IDxcBlob> pixelShaderBlob_ = nullptr;
+
+	//RootSignature作成
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_ = nullptr;
+	
+	//PSOを生成する
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_ = nullptr;
+	
 };
