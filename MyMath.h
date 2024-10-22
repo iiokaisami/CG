a199f96a67aa4f26db8df446d0c562629ad87723
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-22 14:22:52 +0900

    途中

--- MyMath.h
+++ MyMath.h
@@ -93,160 +93,35 @@ namespace MyMath {
 	Matrix4x4 MakeRotateYMatrix(float radian);
 
 	//z軸回転行列
-	Matrix4x4 MakeRotateZMatrix(float radian)
-	{
-		Matrix4x4 m = {
-			std::cos(radian),std::sin(radian),0,0,
-			-std::sin(radian),std::cos(radian),0,0,
-			0,0,1,0,
-			0,0,0,1
-		};
-		return m;
-	}
-
+	Matrix4x4 MakeRotateZMatrix(float radian);
+	
 	//4x4行列の和
-	Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2)
-	{
-		Matrix4x4 result;
-		for (int row = 0; row < 4; row++)
-		{
-			for (int col = 0; col < 4; col++)
-			{
-				result.m[row][col] = m1.m[row][col] + m2.m[row][col];
-			}
-		}
-
-		return  result;
-	}
+	Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
 
 	//4x4行列の積
-	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
-	{
-		Matrix4x4 result;
-		result =
-		{
-			(m1.m[0][0] * m2.m[0][0]) + (m1.m[0][1] * m2.m[1][0]) + (m1.m[0][2] * m2.m[2][0]) + (m1.m[0][3] * m2.m[3][0]), (m1.m[0][0] * m2.m[0][1]) + (m1.m[0][1] * m2.m[1][1]) + (m1.m[0][2] * m2.m[2][1]) + (m1.m[0][3] * m2.m[3][1]), (m1.m[0][0] * m2.m[0][2]) + (m1.m[0][1] * m2.m[1][2]) + (m1.m[0][2] * m2.m[2][2]) + (m1.m[0][3] * m2.m[3][2]), (m1.m[0][0] * m2.m[0][3]) + (m1.m[0][1] * m2.m[1][3]) + (m1.m[0][2] * m2.m[2][3]) + (m1.m[0][3] * m2.m[3][3]),
-			(m1.m[1][0] * m2.m[0][0]) + (m1.m[1][1] * m2.m[1][0]) + (m1.m[1][2] * m2.m[2][0]) + (m1.m[1][3] * m2.m[3][0]), (m1.m[1][0] * m2.m[0][1]) + (m1.m[1][1] * m2.m[1][1]) + (m1.m[1][2] * m2.m[2][1]) + (m1.m[1][3] * m2.m[3][1]), (m1.m[1][0] * m2.m[0][2]) + (m1.m[1][1] * m2.m[1][2]) + (m1.m[1][2] * m2.m[2][2]) + (m1.m[1][3] * m2.m[3][2]), (m1.m[1][0] * m2.m[0][3]) + (m1.m[1][1] * m2.m[1][3]) + (m1.m[1][2] * m2.m[2][3]) + (m1.m[1][3] * m2.m[3][3]),
-			(m1.m[2][0] * m2.m[0][0]) + (m1.m[2][1] * m2.m[1][0]) + (m1.m[2][2] * m2.m[2][0]) + (m1.m[2][3] * m2.m[3][0]), (m1.m[2][0] * m2.m[0][1]) + (m1.m[2][1] * m2.m[1][1]) + (m1.m[2][2] * m2.m[2][1]) + (m1.m[2][3] * m2.m[3][1]), (m1.m[2][0] * m2.m[0][2]) + (m1.m[2][1] * m2.m[1][2]) + (m1.m[2][2] * m2.m[2][2]) + (m1.m[2][3] * m2.m[3][2]), (m1.m[2][0] * m2.m[0][3]) + (m1.m[2][1] * m2.m[1][3]) + (m1.m[2][2] * m2.m[2][3]) + (m1.m[2][3] * m2.m[3][3]),
-			(m1.m[3][0] * m2.m[0][0]) + (m1.m[3][1] * m2.m[1][0]) + (m1.m[3][2] * m2.m[2][0]) + (m1.m[3][3] * m2.m[3][0]), (m1.m[3][0] * m2.m[0][1]) + (m1.m[3][1] * m2.m[1][1]) + (m1.m[3][2] * m2.m[2][1]) + (m1.m[3][3] * m2.m[3][1]), (m1.m[3][0] * m2.m[0][2]) + (m1.m[3][1] * m2.m[1][2]) + (m1.m[3][2] * m2.m[2][2]) + (m1.m[3][3] * m2.m[3][2]), (m1.m[3][0] * m2.m[0][3]) + (m1.m[3][1] * m2.m[1][3]) + (m1.m[3][2] * m2.m[2][3]) + (m1.m[3][3] * m2.m[3][3])
-		};
-
-		return result;
-	}
+	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 
 	//アフィン変換
-	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate)
-	{
-		//平行移動の4x4行列の生成
-		Matrix4x4 scaleMatrix = {
-			scale.x,0,0,0,
-			0,scale.y,0,0,
-			0,0,scale.z,0,
-			0,0,0,1
-		};
-
-
-		//回転で使う4x4行列の宣言
-		Matrix4x4 rotateMatrix;
-		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);		//X軸の回転
-		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);		//Y軸の回転
-		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);		//Z軸の回転
-
-
-		//回転の合成
-		rotateMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
-
-
-		//平行移動の4x4行列の生成
-		Matrix4x4 translateMatrix = {
-			1,0,0,0,
-			0,1,0,0,
-			0,0,1,0,
-			translate.x,translate.y,translate.z,1
-		};
-
-
-		//3次元のアフィン変換
-		Matrix4x4 weight = Multiply(Multiply(scaleMatrix, rotateMatrix), translateMatrix);
-
-		//アフィン変換した値を返す
-		return weight;
-
-	}
-
+	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+	
 	Matrix4x4 Inverse(const Matrix4x4& m);
 	
-
 	float cot(float a) { return cos(a) / sin(a); }
 
 	//正規化
-	Vector3 Normalize(const Vector3& v)
-	{
-		Vector3 result;
-		result.x = v.x / sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-		result.y = v.y / sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-		result.z = v.z / sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-		return result;
-	}
-
+	Vector3 Normalize(const Vector3& v);
+	
 	//透視投影行列
-	Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
-	{
-		Matrix4x4 m{
-		1 / aspectRatio * cot(fovY / 2), 0, 0, 0,
-			0, cot(fovY / 2), 0, 0,
-			0, 0, farClip / (farClip - nearClip), 1,
-			0,0,(-nearClip * farClip) / (farClip - nearClip),0
-		};
-		return m;
-	}
+	Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
 
 	//正射影行列
-	Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip)
-	{
-		Matrix4x4 m{
-			2 / (right - left),0,0,0,
-			0,2 / (top - bottom),0,0,
-			0,0,1 / (farClip - nearClip),0,
-			(left + right) / (left - right),(top + bottom) / (bottom - top),nearClip / (nearClip - farClip),1
-		};
-
-		return m;
-	}
+	Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
 
 	//ビューポート変換行列
-	Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth)
-	{
-		Matrix4x4 m{
-			width / 2,0,0,0,
-			0,-height / 2,0,0,
-			0,0,maxDepth - minDepth,0,
-			left + (width / 2),top + (height / 2),minDepth,1
-		};
-		return m;
-	}
-
-
-	Matrix4x4 MakeScaleMatrix(Vector3 scale)
-	{
-		Matrix4x4 result = {
-			scale.x,0.0f,0.0f,0.0f,
-			0.0f,scale.y,0.0f,0.0f,
-			0.0f,0.0f,scale.z,0.0f,
-			0.0f,0.0f,0.0f,1.0f
-		};
-		return result;
-	}
-
-	Matrix4x4 MakeTranslateMatrix(Vector3 translate)
-	{
-		Matrix4x4 result = {
-			1.0f,0.0f,0.0f,0.0f,
-			0.0f,1.0f,0.0f,0.0f,
-			0.0f,0.0f,1.0f,0.0f,
-			translate.x,translate.y,translate.z,1.0f
-		};
-		return result;
-	}
+	Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+	
+	Matrix4x4 MakeScaleMatrix(Vector3 scale);
+	
+	Matrix4x4 MakeTranslateMatrix(Vector3 translate);
 
 }
--- Sprite.cpp
+++ Sprite.cpp
@@ -1,5 +1,17 @@
 #include "Sprite.h"
 
-void Sprite::Initialize()
+#include "SpriteCommon.h"
+
+void Sprite::Initialize(SpriteCommon* spriteCommon)
 {
+	spriteCommon_ = spriteCommon;
+
+	vertexResource_ = spriteCommon_->GetDxCommon()->CreateBufferResource(sizeof(VertexData) * 4);
+
+	VertexData* vertexData_ = nullptr;
+	vertexResource_->Map(
+		0,
+		nullptr,
+		reinterpret_cast<void**>(&vertexData_)
+	);
 }
