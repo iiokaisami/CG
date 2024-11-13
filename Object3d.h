0c946054fb05a9558ea309657c2c7c34f26da01b
iiokaisami <k023g0025@g.neec.ac.jp>
2024-11-13 10:11:32 +0900

    06_01完成

---
+++ Object3d.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Object3d
+{
+public:
+
+	// 初期化
+	void Initialize();
+
+};
+
---
+++ Object3dCommon.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Object3dCommon
+{
+public:
+
+	// 初期化
+	void Initialize();
+};
+
