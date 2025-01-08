c8666982a0bf45d1d4ec160203eba7d686b1c76d
iiokaisami <k023g0025@g.neec.ac.jp>
2025-01-08 14:32:32 +0900

    11_01完成

--- project/main.cpp
+++ project/main.cpp
@@ -62,26 +62,26 @@
 
 
 
-class ResourceObject
-{
-public:
-	ResourceObject(ID3D12Resource* resource)
-		:resource_(resource)
-	{}
-
-	~ResourceObject()
-	{
-		if (resource_)
-		{
-			resource_->Release();
-		}
-	}
-
-	ID3D12Resource* Get() { return resource_; }
-
-private:
-	ID3D12Resource* resource_;
-};
+//class ResourceObject
+//{
+//public:
+//	ResourceObject(ID3D12Resource* resource)
+//		:resource_(resource)
+//	{}
+//
+//	~ResourceObject()
+//	{
+//		if (resource_)
+//		{
+//			resource_->Release();
+//		}
+//	}
+//
+//	ID3D12Resource* Get() { return resource_; }
+//
+//private:
+//	ID3D12Resource* resource_;
+//};
 
 //depthStencilResource
 
---
+++ project/scene/base/MyGame.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class MyGame
+{
+public:
+
+	// 初期化
+	void Initialize();
+
+	// 終了
+	void Finalize();
+
+	// 更新
+	void Update();
+
+	// 描画	
+	void Draw();
+
+
+};
+
