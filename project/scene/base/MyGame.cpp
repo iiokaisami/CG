e28c6ca9eac489046dc53f16e4f4f9d99c160632
iiokaisami <k023g0025@g.neec.ac.jp>
2025-01-08 14:43:04 +0900

    11_02完成

--- project/scene/base/MyGame.cpp
+++ project/scene/base/MyGame.cpp
@@ -1,7 +1,177 @@
 #include "MyGame.h"
 
+
+
 void MyGame::Initialize()
 {
+
+	
+
+	// WindowsAPIの初期化
+	winApp = new WinApp();
+	winApp->Initialize();
+
+	// DirectXの初期化
+	dxCommon = new DirectXCommon();
+	dxCommon->Initialize(winApp);
+
+	// スプライト共通部分の初期化
+	spriteCommon = new SpriteCommon();
+	spriteCommon->Initialize(dxCommon);
+
+	// 3Dオブジェクト共通部の初期化
+	Object3dCommon::GetInstance()->Initialize(dxCommon);
+
+	// モデル共通部分の初期化
+	modelCommon = new ModelCommon();
+	modelCommon->Initialize(dxCommon);
+
+	// 3Dモデルマネージャーの初期化
+	ModelManager::GetInstance()->Initialize(dxCommon);
+
+	// SRVマネージャーの初期化
+	srvManager = new SrvManager();
+	srvManager->Initialize(dxCommon);
+
+
+
+
+
+
+
+#ifdef _DEBUG
+
+	imGuiManager = new ImGuiManager();
+	imGuiManager->Initialize(winApp, dxCommon);
+
+#endif // _DEBUG
+
+
+	// テクスチャマネージャーの初期化
+	TextureManager::GetInstance()->Initialize(dxCommon, srvManager);
+
+
+	// デバイス
+	Microsoft::WRL::ComPtr<ID3D12Device> device = dxCommon->GetDevice();
+	// コマンドアロケータ
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator = dxCommon->GetCommandAllocator();
+	// コマンドリスト
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList = dxCommon->GetCommandList();
+	// DescriptorHeap
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvDescriptorHeap = dxCommon->GetSrvDescriptorHeap();
+	// descriptorSizeSRV
+	uint32_t descriptorSizeSRV = dxCommon->GetDescriptorSizeSRV();
+	// ビューポート
+	D3D12_VIEWPORT viewport = dxCommon->GetViewport();
+	// シザー矩形
+	D3D12_RECT scissorRect = dxCommon->GetScissorRect();
+
+
+
+
+
+
+
+
+
+
+
+
+	// ポインタ
+	Input::GetInstance()->Initialize(winApp);
+
+
+
+
+	//////////////////////////////////////
+
+	// カメラマネージャーテスト
+
+	auto& cameraManager = CameraManager::GetInstance();
+
+	// カメラの作成
+	auto camera1 = std::make_shared<Camera>();
+	auto camera2 = std::make_shared<Camera>();
+
+	Vector3 camera1Rotate = camera1->GetRotate();
+	Vector3 camera1Position = camera1->GetPosition();
+	camera1Position.z = -5.0f;
+	Vector3 camera2Rotate = camera2->GetRotate();
+	Vector3 camera2Position = camera2->GetPosition();
+	camera2Position.z = -10.0f;
+
+	// カメラの追加
+	cameraManager.AddCamera(camera1);
+	cameraManager.AddCamera(camera2);
+
+	// アクティブカメラの設定
+	cameraManager.SetActiveCamera(0);
+	uint32_t activeIndex = cameraManager.GetActiveIndex();
+
+	Object3dCommon::GetInstance()->SetDefaultCamera(cameraManager.GetActiveCamera());
+
+
+	//////////////////////////////////////
+
+
+
+	//ModelManager::GetInstance()->LoadModel("plane.obj");
+	//ModelManager::GetInstance()->LoadModel("axis.obj");
+
+	//// 3Dオブジェクトの初期化
+	//std::vector<Object3d*>object3ds;
+
+	//for (uint32_t i = 0; i < 2; ++i) {
+	//	Object3d* object = new Object3d();
+	//	if (i == 0) {
+	//		object->Initialize("plane.obj");
+	//	}
+	//	if (i == 1) {
+	//		object->Initialize("axis.obj");
+	//	}
+
+	//	Vector3 position;
+	//	position.x = i * 2.0f;
+	//	object->SetPosition(position);
+
+	//	object3ds.push_back(object);
+	//}
+
+
+
+
+
+
+
+	//////////////////////////////////////
+	
+
+	std::vector<Sprite*> sprites;
+	for (uint32_t i = 0; i < 1; ++i)
+	{
+		Sprite* sprite = new Sprite();
+		if (i == 0 || i == 3) {
+			sprite->Initialize(spriteCommon, "uvChecker.png");
+		}
+		else {
+			sprite->Initialize(spriteCommon, "monsterBall.png");
+		}
+		sprites.push_back(sprite);
+
+		position = sprite->GetPosition();
+		// 座標を変更する
+		position.x = 100.0f;
+		position.y = 100.0f;
+		// 変更を反映する
+		sprite->SetPosition(position);
+
+		Vector2 size = sprite->GetSize();
+		size.x = 370.0f;
+		size.y = 370.0f;
+		sprite->SetSize(size);
+	}
+
+	//////////////////////////////////////
 
 }
 
--- project/scene/base/MyGame.h
+++ project/scene/base/MyGame.h
@@ -1,5 +1,44 @@
 #pragma once
 
+#include <Windows.h>
+
+#include <format>
+#include <cassert>
+#include <numbers>
+#include <vector>
+#include <DirectXMath.h>
+#include <fstream>
+#include <sstream>
+
+#include "Input.h"
+#include "WinApp.h"
+#include "DirectXCommon.h"
+#include "SpriteCommon.h"
+#include "Sprite.h"
+#include "MyMath.h"
+#include "TextureManager.h"
+#include "Object3dCommon.h"
+#include "Object3d.h"
+#include "ModelCommon.h"
+#include "Model.h"
+#include "ModelManager.h"
+#include "SrvManager.h"
+
+/// パーティクルテスト///
+
+#include "../ParticleManager.h"
+#include "../ParticleEmitter.h"
+#include <iostream>
+
+/////////////////////////
+
+#ifdef _DEBUG
+
+#include "../ImGuiManager.h"
+#include "../imgui/imgui.h"
+
+#endif // _DEBUG
+
 class MyGame
 {
 public:
@@ -16,6 +55,33 @@ public:
 	// 描画	
 	void Draw();
 
+private:
+
+
+	// ポインタ
+	WinApp* winApp = nullptr;
+	DirectXCommon* dxCommon = nullptr;
+	SpriteCommon* spriteCommon = nullptr;
+	ModelCommon* modelCommon = nullptr;
+	SrvManager* srvManager = nullptr;
+
+
+#ifdef _DEBUG
+
+	ImGuiManager* imGuiManager = nullptr;
+
+#endif // _DEBUG
+
+
+
+	Vector2 position{};
+
+
+	bool isFilipX = false;
+	bool isFilipY = false;
+
+	Vector2 textureLeftTop{};
+	Vector2 textureSize{ 500.0f ,500.0f };
 
 };
 
