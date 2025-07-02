e04189d0bf21ca63286d7b35cae7173a3bbbb442
iiokaisami <k023g0025@g.neec.ac.jp>
2025-07-02 14:19:18 +0900

    オフスクリーン確認に不必要なゲーム要素削除

--- project/scene/scene/GamePlayScene.h
+++ project/scene/scene/GamePlayScene.h
@@ -3,11 +3,6 @@
 #include <Framework.h>
 
 #include "../base/BaseScene.h"
-#include "../../application/Objects/Player/Player.h"
-#include "../../application/Objects/Enemy/EnemyManager.h"
-#include "../../application/Objects/Field/Field.h"
-#include "../../application/Objects/Field/Wall.h"
-#include "../../application/Collider/ColliderManager.h"
 
 class GamePlayScene : public BaseScene
 {
@@ -25,25 +20,12 @@ public:
 	// 描画
 	void Draw() override;
 
-	// カメラ処理
-	void CameraUpdate();
-
-	// カメラシェイク
-	void CameraShake();
-
-	// カメラ追尾
-	void CameraFollowZoom();
-
-
 private:
 
 	CameraManager& cameraManager = CameraManager::GetInstance();
 	std::shared_ptr<Camera> camera1 = nullptr;
 	std::shared_ptr<Camera> camera2 = nullptr;
     
-	// 衝突判定
-	ColliderManager* colliderManager_ = nullptr;
-
     uint32_t activeIndex = 0;
 	Vector3 camera1Rotate = { -0.9f,0.0f,0.0f };
 	Vector3 camera1Position = { 0.0f,-20.0f,0.0f };
@@ -53,17 +35,6 @@ private:
 	// 2Dスプライト
 	//std::vector<Sprite*>sprites = {};
 
-	// プレイヤー
-	std::unique_ptr<Player> pPlayer_ = nullptr;
-
-	// エネミー
-	std::unique_ptr<EnemyManager> pEnemyManager_ = nullptr;
-
-	// フィールド
-	std::unique_ptr<Field> pField_ = nullptr;
-
-	// 壁
-	std::vector<std::unique_ptr<Wall>> pWalls_;
 
 	
 	// カメラが静止時に追従する基準位置
