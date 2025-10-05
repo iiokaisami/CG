1ff8a3f04aa2e5d60d635da99ee43e0bcc2e6130
iiokaisami <k023g0025@g.neec.ac.jp>
2025-10-05 19:49:51 +0900

    タイトルバーゲーム名に変更

--- project/gameEngine/base/WinApp.cpp
+++ project/gameEngine/base/WinApp.cpp
@@ -52,7 +52,7 @@ void WinApp::Initialize()
 	//ウィンドウの設定
 	hwnd = CreateWindow(
 		wc.lpszClassName,		//利用するクラス名
-		L"CG",					//タイトルバーの文字
+		L"ウェーブシューター",	//タイトルバーの文字
 		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
 		CW_USEDEFAULT,			//表示X座標(WindowsOSに任せる)
 		CW_USEDEFAULT,			//表示Y座標(WindowsOSに任せる)
